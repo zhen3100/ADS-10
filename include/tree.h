2997b80916ef4f9467605c3d8488814907111b37
Anton A. Shtanyuk <ashtanyuk@gmail.com>
2022-05-08 20:02:26 +0300

    Initial commit

---
+++ include/tree.h
@@ -0,0 +1,5 @@
+// Copyright 2022 NNTU-CS
+#ifndef INCLUDE_TREE_H_
+#define INCLUDE_TREE_H_
+
+#endif  // INCLUDE_TREE_H_
---
+++ src/alg.cpp
@@ -0,0 +1,8 @@
+// Copyright 2022 NNTU-CS
+#include  <iostream>
+#include  <fstream>
+#include  <locale>
+#include  <cstdlib>
+#include  "tree.h"
+
+
