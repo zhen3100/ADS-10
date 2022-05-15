628e1b043bbffb6ac1fc864ea1ceaaba01c81b73
Anton A. Shtanyuk <ashtanyuk@gmail.com>
2022-05-15 12:55:24 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 // Copyright 2022 NNTU-CS
 #include "tree.h"
 
+std::vector<char> getPerm(Tree& tree, int n);
+
 int main() {
-    return 0;
+    
+  // определяем входной вектор для дерева
+  std::vector<char> in = {'1','2','3'};
+
+  // создаем дерево
+  Tree tree(in);
+
+  // запрашиваем первую перестановку
+  std::vector<char> result1 = getPerm(tree, 1);  //  123
+
+  // запрашиваем вторую перестановку
+  std::vector<char> result2 = getPerm(tree, 2);  //  132
+    
+  return 0;
 }
