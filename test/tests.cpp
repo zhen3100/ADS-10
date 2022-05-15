dc895bcdb420d5343ff6fdb8ffd5d60c222b28e8
Anton A. Shtanyuk <ashtanyuk@gmail.com>
2022-05-15 13:09:36 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -9,6 +9,6 @@ std::vector<char> getPerm(const Tree& tree, int n);
 TEST(ads10, test1) {
   Tree tree(std::vector<char>{'1', '2', '3'});
   std::vector<char> result = getPerm(tree, 1);
-  ASSERT_TRUE( result[0] == '1' && result[1] == '2' && result[2] == '3');
+  ASSERT_TRUE(result[0] == '1' && result[1] == '2' && result[2] == '3');
 }
      
