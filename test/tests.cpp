2b4d42c7a2f44e88848810675872ad048af45500
Anton A. Shtanyuk <ashtanyuk@gmail.com>
2022-05-15 13:13:38 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -10,4 +10,22 @@ TEST(ads10, test1) {
   Tree tree(std::vector<char>{'1', '2', '3'});
   std::vector<char> result = getPerm(tree, 1);
   ASSERT_TRUE(result[0] == '1' && result[1] == '2' && result[2] == '3');
-}     
+}
+
+TEST(ads10, test2) {
+  Tree tree(std::vector<char>{'1', '2', '3'});
+  std::vector<char> result = getPerm(tree, 2);
+  ASSERT_TRUE(result[0] == '1' && result[1] == '3' && result[2] == '2');
+}
+
+TEST(ads10, test3) {
+  Tree tree(std::vector<char>{'1', '2', '3'});
+  std::vector<char> result = getPerm(tree, 6);
+  ASSERT_TRUE(result[0] == '3' && result[1] == '2' && result[2] == '1');
+}
+
+TEST(ads10, test4) {
+  Tree tree(std::vector<char>{'1', '2', '3'});
+  std::vector<char> result = getPerm(tree, 8);
+  ASSERT_TRUE(result.size() == 0);
+}
